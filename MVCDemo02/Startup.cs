d2924f70f8bd189929fd7bd9749f947fc66c0f87
shibo hu <shibohu@hotmail.com>
2015-11-17 21:13:44 +0800

    first check in

---
+++ MVCDemo02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCDemo02.Startup))]
+namespace MVCDemo02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
